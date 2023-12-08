d75ffd32639df94b742213ede8bf245d08cd9980
jrcosta23 <jose.rcosta3@senacsp.edu.br>
2023-12-08 10:12:56 -0300

    Atualização do database padaria

--- dbpadaria.sql
+++ dbpadaria.sql
@@ -1,195 +1,125 @@
 show databases;
- 
- create database dbpadaria;
- 
- use dbpadaria;
- 
- 
- create table Fornecedores (
- idFornenecedor int primary key  auto_increment,
- nome varchar (50),
- cnpj varchar (20), 
- tel varchar(20),
- email varchar(20),
- cep varchar (9),
- endereco varchar (100),
- numero varchar (10),
- bairro varchar (40),
- cidade varchar (40),
- estado char (2)
- );
- 
- select * from Fornecedores;
- 
-insert into Fornecedores( nome, cnpj, tel, email, cep, endereco, numero, bairro, cidade, estado) values 
-("junior de andrade", "46.712.002-79",  "(11) 99645-3271", "junior@gmail.com", "04923-200"," polo da paz","21", "nakamura","sao paulo","sp");
 
- 
+create database dbpadaria;
 
- create table Produtos (
- idProduto int primary key   auto_increment,
- nomeProdutos varchar(50),
- descricao text, 
- precoProdutos Decimal (10,2),
- estoqueProduto int,
- categoriaProduto enum ( 'pães','café', 'salgados') ,
- idFornecedores  int
- );
- 
-describe Produtos;
- 
- insert into Produtos (nomeProdutos, precoProdutos, estoqueProduto, categoriaProduto, idFornecedores) values ('pãe de forma' ,15.00 , 1.000, 'pães', 1);
-  insert into Produtos (nomeProdutos, precoProdutos, estoqueProduto, categoriaProduto, idFornecedores) values ('pãe de mel ' ,5.00, 10, 'pães', 1);
- 
-select * from Produtos;
+use dbpadaria;
 
-insert into Produtos (nomeProdutos, precoProdutos, estoqueProduto, categoriaProduto, idFornecedores) values ('pãe de forma' ,15.00 , 1.000, 'pães', 1);
- insert into Produtos (nomeProdutos, precoProdutos, estoqueProduto, categoriaProduto, idFornecedores) values ('pãe de mel ' ,5.00, 10, 'pães', 1);
- 
- 
-insert into Produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor) values
-("Bolo de Chocolate", "Fofinho, prático, rápido e delicioso: essa é realmente a melhor receita de bolo de chocolate do mundo!", "45.00", 10, "Bolos", 1);
-
-insert into Produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor) values
-("Pão Francês", "O pãozinho é algo que não sai da dieta das pessoas. Seja no café da manhã, como acompanhamento antes de um almoço ou como lanche da tarde.", "0.50", 50, "Pães", 1);
-
-alter table Produtos ADD column validadeProduto datetime;
-alter table Produtos ADD column pesoProduto decimal(10,2);
-alter table Produtos ADD column ingredientesProduto text;
-
-insert into Produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor, pesoProduto, ingredientesProduto, validadeProduto) values
-("Bolo de Chocolate", "Fofinho, prático, rápido e delicioso: essa é realmente a melhor receita de bolo de chocolate do mundo!", "45.00", 10, "Bolos", 1, "2.00", "2 xícaras de farinha de trigo, 1 xícara de leite, 1 colher (sopa) de fermento em pó, cobertura, 2 xícaras de leite, 2 xícaras de açucar, 6 colheres (sopa) de chocolate em pó, 6 ovos, 2 colheres (sopa) de manteiga", "2023-11-20");
-
-insert into Produtos (nomeProduto, descricaoProduto, precoProduto, estoqueProduto, categoriaProduto, idFornecedor, pesoProduto, ingredientesProduto, validadeProduto) values
-("Pão Francês", "O pãozinho é algo que não sai da dieta das pessoas. Seja no café da manhã, como acompanhamento antes de um almoço ou como lanche da tarde.", "0.00", 50, "Pães", 1, "0.45", "1/2 kg de farinha de trigo, 15g de sal, 1 colher (sopa) de margarina, 10g de fermento para pão, 20g de açucar", "2023-11-16");
-
-
-update Produtos set pesoProduto = "2.00" where idProduto = 1;
-update Produtos set ingredientesProduto = "2 xícaras de farinha de trigo, 1 xícara de leite, 1 colher (sopa) de fermento em pó, cobertura, 2 xícaras de leite, 2 xícaras de açucar, 6 colheres (sopa) de chocolate em pó, 6 ovos, 2 colheres (sopa) de manteiga" where idProduto = 1;
-update Produtos set validadeProduto = "2023-11-20" where idProduto = 1;
-
-update Produtos set pesoProduto = "0.45" where idProduto = 2;
-update Produtos set ingredientesProduto = "1/2 kg de farinha de trigo, 15g de sal, 1 colher (sopa) de margarina, 10g de fermento para pão, 20g de açucar" where idProduto = 2;
-update Produtos set validadeProduto = "2023-11-15" where idProduto = 2;
-
-
-select * from Produtos;
-select * from Produtos where categoriaProduto = "Pães";
-select * from Produtos where precoProdutos < 50.00 order by precoProdutos asc;
- 
- 
-select * from Produtos;
- 
- 
- select sum(quantidade * 15.00) as total from itensPedido inner join Produtos on itensPedido.idProdutos = 
- Produtos.idProduto where idPedido = 1;
- 
-
-
-  
- create table Pedido (
-   idpedido int primary key auto_increment,
-   dataPedido timestamp default current_timestamp,
-   stastusPedido enum("pendente", "finalizado", "cancelado") not null,
-   idCliente int  not null,
-   foreign key (idCliente)  references Clientes(idCliente)
+create table Fornecedores (
+idFornenecedor int primary key auto_increment ,
+nome varchar(50) not null,
+cnpj varchar(20) not null,
+tel varchar(20),
+email varchar(20) not null unique,
+cep varchar(9),
+endereco varchar(100),
+numero varchar(10),
+bairro varchar(40),
+cidade varchar(40),
+estado char(2)
 );
- describe Pedido;
- insert  into  Pedido (startusPedido, idCliente) values ("pendente",  1);
- insert  into  Pedido (startusPedido, idCliente) values ("finalizado", 4);
- 
- select * from pedido;
- 
 
- 
-create table Clientes(
+insert into Fornecedores(nome, cnpj, tel, email, cep, endereco, numero, bairro, cidade, estado) values
+("Emily", "56.649.102/0001-97", "(11) 2757-6155", "contato@emily.com.br", "13306-050", "Rua Luiz Morato Castanho", "631", "Rancho Grande", "Itu", "SP");
+
+select * from Fornecedores;
+
+
+
+create table Produtos (
+idProduto int primary key auto_increment,
+nomeProdutos varchar(50) not null,
+descricao text,
+precoProdutos decimal (10,2) not null,
+estoqueProduto int not null,
+categoriaProduto enum ('pães','salgados','café'),
+validadeProdutos datetime,
+ingredientesProduto text,
+idFornenecedor int not null,
+foreign key (idFornenecedor) references Fornecedores(idFornenecedor)
+);
+
+describe Produtos;
+
+
+
+insert into Produtos(nomeProdutos, descricao, precoProdutos, estoqueProduto, categoriaProduto,validadeProdutos,ingredientesProduto, idFornenecedor)
+values ("Salsichão", "SALSICHAO FGO TOM SECO EDER FAT 100G", 2.50, 12, "salgados", "2023-12-24", "farinha, salsicha e sal", 1);
+
+
+
+insert into Produtos(nomeProdutos, descricao, precoProdutos, estoqueProduto, categoriaProduto,validadeProdutos,ingredientesProduto, idFornenecedor)
+values ("Empadão", "salgadinho de massa recheada de galinha, queijo, camarão, palmito etc., assada ao forno em fôrma.", 14.50, 8, "salgados", "2023-12-24", "farinha, frango e requeijão", 1);
+
+
+select * from Produtos;
+
+select * from Produtos where categoriaProduto = "salgados";
+
+select * from Produtos where precoProdutos < 10.00 order by precoProdutos asc;
+
+
+create table clientes (
 idCliente int primary key auto_increment,
-nomeCliente varchar (50),
-cpfCliente varchar (15),
-telefoneCliente varchar (20),
-emailCliente varchar(50),
-cep varchar (9),
-enderecoCliente varchar (100),
-bairro varchar (40),
-estado char (2)
-
+nomeCliente varchar(50),
+cpfCliente varchar(15) not null unique,
+telefoneCliente varchar(20),
+emailCliente varchar(50) unique,
+cep varchar(9),
+enderecoCliente varchar(100),
+bairro varchar(40), 
+estado varchar(2)
 );
 
-select * from Clientes;
+insert into clientes (nomeCliente, cpfCliente, telefoneCliente,emailCliente,cep,enderecoCliente,bairro,estado) values ("Melissa Heloisa", "919.810.370-98", "(61) 3728-7811", "melissa@universo3d.com.br", "72260-807", "Quadra QNO 18 Conjunto 7", "Ceilândia Norte", "DF");
 
-insert into Clientes( nomeCliente , cpfCliente , telefoneCliente , emailCliente)values("junior de andrade",  "46.712.002-79","(11) 99645-3271", "junior@gmail.com");
+select * from clientes;
 
 
-create table  itensPedido (
+create table pedido (
+idpedido int primary key auto_increment,
+datapedido timestamp default current_timestamp,
+stastuspedido enum('pendente','finalizado','cancelado'),
+idCliente int not null,
+foreign key (idCliente) references clientes(idCliente)
+);
+
+
+select * from pedido;
+
+insert into pedido (stastuspedido, idCliente) values ("finalizado", 1);
+
+select * from pedido inner join clientes on pedido.idCliente = clientes.idCliente;
+
+create table itensPedido (
 iditensPedido int primary key auto_increment,
-idPedido int not null,
-foreign key (idPedido) references Pedido (idPedido),
+idpedido int not null,
+foreign key (idpedido) references pedido(idpedido),
 idProduto int not null,
-foreign key (idProduto) references Produtos (idProduto),
+foreign key (idProduto) references produtos(idProduto),
 quantidade int not null
-
 );
 
-describe itensPedido;
 
-insert into itensPedido(idpedido, idProduto, quantidade) values (1,1,3);
-insert into itensPedido(idpedido, idProduto, quantidade) values (1,2,5);
+insert into itensPedido (idpedido, idProduto, quantidade) values (1, 8, 2);
 
+insert into itensPedido (idpedido, idProduto, quantidade) values (1, 9, 1);
 
-select  *  from   pedido   inner join Cliente on pedido.idCliente =  cliente.idCliente;
-select  * from    itensPedido inner join pedido on itnesPedido.idPedido = pedido.idPedido;
-select  * from    itensPedido inner join pedido  on itensPedido.idPedido = produtos.idProduto;
-select  * from    itens Pedido inner join produto on itensPedido.idPedido = pedido.idPedido;
+select * from itensPedido;
+
+select sum(Produtos.precoProdutos * itensPedido.quantidade) as Total from Produtos inner join itensPedido on Produtos.idProduto = itensPedido.idProduto where idPedido = 1;
 
 
-/*OBJETIVO: consultar o nome do cliente que abriu seu pedido em  determinada data para  verificar  o que  ele comprou de itens nesse pedido  e a respectiva quantidade,
-ou seja, quais produtos e quanto  cada um 	quem comprou  o que quanto comprou*/
-
-
- select cliente.nomeCliente, pedido.idPedido, pedido.dataPedido,  itensPedido.quantidade, produto.precoProduto
-from Clientes  inner join pedido on pedido.idCliente = pedido.idCliente inner join
-itensPedido on pedido.idPedido = itensPedido.idPedido inner join
-produto on  produto. idPedido.idProduto;
-
-
-select sum(Produtos.precoProdutos *  itensPedido.quantidade) as total from Produtos  inner join itensPedido on Produtos.idProduto =  idPedido.idProduto where  idPedido = 1;
-
-
-
-/* FILTRAR PRODUTOS  POR  VALIDADE (POR EXEMPLO, PRODUTOS COM VALIDADE MAIOR  QUE A DATA ATUAL) */
-select * from Produtos where validadeProdutos  >   curdate();
-
-
-
-/*FILTRAR PRODUTOS QUE CONTENHAS  UM INGREDIENTE  ESPECÍFICO  */
-
-select * from  Produtos  where ingredientesProduto like "%glúten%";
-
-
-
-/* ATIVIDADE: FILTRAR  PÃES QUE SEJAM FEITO S Á BASE DE FARINHA DE TRIGO, COM VALOR ATÉ 7.90 */
-/* QUERO UM RESULTADO  AO  MENOS! */
-
-describe Produtos;
+/* Filtrar produtos por validade (por exemplo, produtos com validade maior do que a data atual) */
+ select * from Produtos where validadeProdutos > curdate();
  
- /*ATIVIDADE: FILTRAR PÃES NÃO QUE SEJAM FEITOS À BASE DE FARINHA DE TRIGO, COM VALOR ATÉ 7.90*/
+ /*Filtrar produtos que contenham um ingrediente específico */
+ select * from Produtos where ingredientesProduto like '%farinha%';
  
- select * from Produtos where categoriaProduto = 'Pães' and ingredientesProduto not like '%farinha de trigo%' and precoProdutos <= 7.90;
- 
-insert into Produtos (nomeProdutos, ingredientesProduto, pesoProdutos, validadeProdutos, precoProdutos, estoqueProduto, categoriaProduto, idFornecedores) values
-("Bolinho de queijo", "sem lactose", 120, "2023-12-07", 13.50, 5, "salgados", 1); 
-
-insert into Produtos (nomeProdutos, ingredientesProduto, pesoProduto, validadeProdutos, precoProdutos,estoqueProduto, categoriaProduto, idFornecedores) values
-("Pão italiano", "trigo", 0.450, "2023-12-07", 7.90, 5, "coxinha", 1);
+ /*ATIVIDADE: FILTRAR PÃES QUE SEJAM FEITOS À BASE DE FARINHA DE TRIGO, COM VALOR ATÉ 7.90*/
+ select * from Produtos where categoriaProduto = 'salgados' and ingredientesProduto like '%farinha%' and precoProdutos <= 7.90;
 
 
-
-update itensPedido inner join Produtos on  itensPedido.idProduto = Produtos.idProduto
-set Produtos.estoqueProduto = Produtos.estoqueProduto - itensPedido.quantidade 
-where itensPedido.quantidade > 0;
-
-select nomeProdutos, estoqueProduto from  Produtos;
-
-
-
-
+select pedido.idpedido, Produtos.idProduto, Clientes.nomeCliente, Produtos.nomeProdutos, itensPedido.quantidade, Produtos.precoProdutos
+from pedido inner join Clientes on pedido.idCliente = Clientes.idCliente inner join 
+itensPedido on pedido.idpedido = itensPedido.idpedido inner join 
+Produtos on Produtos.idProduto = itensPedido.idProduto;
